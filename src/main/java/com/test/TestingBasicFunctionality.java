3daa160994a54a12c46322be2c1d719a78c3610c
Crosslywere <ogboruj@outlook.com>
2025-06-17 20:47:26 +0100

    Changed code to use new systems

--- src/main/java/com/test/TestingBasicFunctionality.java
+++ src/main/java/com/test/TestingBasicFunctionality.java
@@ -1,133 +1,150 @@
 package com.test;
 
-import com.crossly.CoffeeEngine;
-import com.crossly.components.Framebuffer;
-import com.crossly.components.Model;
-import com.crossly.components.ShaderProgram;
-import com.crossly.components.subcomponents.Transform;
-import com.crossly.entities.Camera3D;
-import com.crossly.entities.Entity;
-import com.crossly.input.Input;
-import com.crossly.interfaces.Application;
-import com.crossly.timer.Timer;
+import com.crossly.coffee_engine.component.*;
+import com.crossly.coffee_engine.core.*;
+import com.crossly.coffee_engine.entity.*;
+import com.crossly.util.FileUtil;
+import org.joml.Vector2f;
 import org.joml.Vector2i;
+import org.joml.Vector3f;
 
 public class TestingBasicFunctionality extends Application {
 
-    private final Entity entity;
-    private final Camera3D camera;
-    private boolean mouseFirst = true;
+    private Camera camera;
+    private Entity model;
+
     private Vector2i mousePosLast = new Vector2i();
 
-    private final float CLAMP_CAM = (float) Math.toRadians(89.9);
-
     public TestingBasicFunctionality() {
-        super(800, 600, "Testing Basic Functionality");
-        ShaderProgram shader = ShaderProgram.builder()
-                .setProjection("uProjection")
-                .setView("uView")
-                .setModel("uModel")
-                .attachVertexShaderFile("shaders/shader.vert")
-                .attachFragmentShaderFile("shaders/shader.frag")
-                .build();
-        shader.use();
-        shader.setFloat3("uColor", 1, 1, 1);
-        Model mesh = new Model("models/Queen.obj");
-        entity = new Entity(shader, mesh);
-        camera = new Camera3D(getWindowWidth(), getWindowHeight());
-        camera.setScreenShader(ShaderProgram.builder()
-                        .attachVertexShader("""
+        setTitle("Testing Basic Functionality");
+        setMonitorIndex(2);
+        createContext();
+        Framebuffer.setDepthTest(true);
+    }
+
+    @Override
+    public void onCreate() {
+        camera = new Camera(getWidth(), getHeight(), new Shader(
+                """
                                 #version 330 core
                                 layout (location = 0) in vec3 aPos;
                                 void main() {
-                                    gl_Position = vec4(aPos, 1.0);
-                                }""")
-                        .attachFragmentShader("""
+                                gl_Position = vec4(aPos, 1.0);
+                                }""",
+                """
                                 #version 330 core
                                 layout (location = 0) out vec4 oColor;
                                 uniform sampler2D render;
                                 uniform vec2 resolution;
                                 #define PIXEL_SIZE 2.0
                                 void main() {
-                                    vec2 uv = floor(gl_FragCoord.xy / PIXEL_SIZE) * PIXEL_SIZE;
-                                    uv /= resolution;
-                                    oColor = texture2D(render, uv);
-                                }""")
-                .build(), "render");
-        camera.getTransform().setPositionY(2.5f);
-        camera.getTransform().setPositionZ(-2.5f);
-        camera.getTransform().setPitch((float) Math.toRadians(35));
-        Framebuffer.cullBackFaces();
-        Framebuffer.enableDepthTest();
-        Framebuffer.enableBlendFuncOMSA_Add();
+                                vec2 uv = floor(gl_FragCoord.xy / PIXEL_SIZE) * PIXEL_SIZE;
+                                uv /= resolution;
+                                oColor = texture2D(render, uv);
+                                }"""), Framebuffer.builder().add2DColor().add2DDepth());
+        camera.getComponent(Transform.class).ifPresent(
+                transform -> {
+                    transform.setPosition(new Vector3f(0, 2.5f, -2.5f));
+                    transform.setRotation(new Vector3f(35, 0, 0));
+                }
+        );
+        camera.setUpdateCallback(new UpdateCallback() {
+            @Override
+            public void invoke(Entity self) {
+                self.getComponent(Transform.class).ifPresent(
+                        transform -> {
+                            Vector3f movement = new Vector3f();
+                            if (Input.isKeyPressed(Input.KEY_W))
+                                movement.z += 1;
+                            if (Input.isKeyPressed(Input.KEY_S))
+                                movement.z -= 1;
+                            if (Input.isKeyPressed(Input.KEY_D))
+                                movement.x += 1;
+                            if (Input.isKeyPressed(Input.KEY_A))
+                                movement.x -= 1;
+                            if (Input.isKeyPressed(Input.KEY_SPACE))
+                                movement.y += 1;
+                            if (Input.isKeyPressed(Input.KEY_LEFT_CTRL))
+                                movement.y -= 1;
+                            if (movement.length() != 0) {
+                                movement.normalize().mul(Timer.getDeltaTime());
+                                transform.setPosition(
+                                        transform.getPosition().add(
+                                                transform.getFront().mul(movement.z()).add(
+                                                        transform.getRight().mul(movement.x()).add(
+                                                                transform.getUp().mul(movement.y())
+                                                        )
+                                                )
+                                        )
+                                );
+                            }
+                            // Camera rotation
+                            if (Input.isButtonPressed(Input.MOUSE_BUTTON_LEFT)) {
+                                var mousePos = Input.getMousePos();
+                                if (Input.isButtonJustPressed(Input.MOUSE_BUTTON_LEFT)) {
+                                    mousePosLast = Input.getMousePos();
+                                }
+                                float xoffset = mousePosLast.x() - mousePos.x();
+                                float yoffset = mousePos.y() - mousePosLast.y();
+                                transform.setRotation(transform.getRotation().add(new Vector3f(yoffset, xoffset, 0)).mul(Timer.getDeltaTime() * 2));
+                                transform.setPitch(Math.clamp(transform.getPitch(), -89, 89));
+                                mousePosLast = mousePos;
+                                Input.disableMouse();
+                            }
+                            if (!Input.isButtonPressed(Input.MOUSE_BUTTON_LEFT)) {
+                                Input.normalMouse();
+                            }
+                        }
+                );
+            }
+        });
+        camera.setRenderCallback(new RenderCallback() {
+            @Override
+            public void invoke(Entity self) {
+                Framebuffer.unbind();
+                var shader = self.getComponent(Shader.class).orElse(null);
+                var framebuffer = self.getComponent(Framebuffer.class).orElse(null);
+                if (shader == null || framebuffer == null) return;
+                shader.use();
+                shader.setFloat2("resolution", new Vector2f(getWidth(), getHeight()));
+                shader.setTexture("render", framebuffer.getTexture(0), 0);
+                StaticMesh.UNIT_RECT.render();
+            }
+        });
+        model = Entity.create(new Shader(FileUtil.getFileString("shaders/shader.vert"), FileUtil.getFileString("shaders/shader.frag")), new StaticMesh("models/Queen.obj"));
+        model.setRenderCallback(new RenderCallback() {
+            @Override
+            public void invoke(Entity self) {
+                self.getComponent(Shader.class).ifPresent((shader) -> {
+                    shader.setMatrix4f("uProjection", camera.getProjection3D());
+                    shader.setMatrix4f("uView", camera.getView());
+                    shader.setMatrix4f("uModel", self.getComponent(Transform.class).get().getModelMatrix());
+                    shader.setFloat3("uColor", new Vector3f(1));
+                    self.getComponent(StaticMesh.class).ifPresent(Mesh::render);
+                });
+            }
+        });
     }
 
     @Override
-    public void onUpdate(Input input) {
-        if (input.isKeyPressed(Input.KEY_ESCAPE)) quit();
-        if (input.isKeyJustPressed(Input.KEY_F)) setFullscreen(!getFullscreen());
-        // Moving the camera based on
-        if (input.isKeyPressed(Input.KEY_W))
-            camera.getTransform().incrementPosition(camera.getTransform().getFront().mul(Timer.getDeltaTime()));
-        if (input.isKeyPressed(Input.KEY_S))
-            camera.getTransform().incrementPosition(camera.getTransform().getFront().mul(-Timer.getDeltaTime()));
-        if (input.isKeyPressed(Input.KEY_A))
-            camera.getTransform().incrementPosition(camera.getTransform().getRight().mul(-Timer.getDeltaTime()));
-        if (input.isKeyPressed(Input.KEY_D))
-            camera.getTransform().incrementPosition(camera.getTransform().getRight().mul(Timer.getDeltaTime()));
-
-        if (input.isKeyPressed(Input.KEY_SPACE))
-            camera.getTransform().incrementPosition(Transform.getWorldUp().mul(Timer.getDeltaTime()));
-        if (input.isKeyPressed(Input.KEY_LEFT_CTRL))
-            camera.getTransform().incrementPosition(Transform.getWorldUp().mul(-Timer.getDeltaTime()));
-
-        // Orienting the camera's roll
-        if (input.isScrollDown())
-            camera.getTransform().setRoll(camera.getTransform().getRoll() + Timer.getDeltaTime());
-        if (input.isScrollUp())
-            camera.getTransform().setRoll(camera.getTransform().getRoll() - Timer.getDeltaTime());
-        if (input.isButtonJustPressed(Input.MOUSE_BUTTON_MIDDLE))
-            camera.getTransform().setRoll(0);
-
-        // Orienting the camera's pitch and yaw
-        if (input.isButtonPressed(Input.MOUSE_BUTTON_LEFT)) {
-            var mousePos = input.getMousePos();
-            if (mouseFirst) {
-                mousePosLast = input.getMousePos();
-                mouseFirst = false;
-            }
-            float xoffset = mousePosLast.x() - mousePos.x();
-            float yoffset = mousePos.y() - mousePosLast.y();
-            camera.getTransform().setPitch(Math.clamp(camera.getTransform().getPitch() + (yoffset * Timer.getDeltaTime() * .05f), -CLAMP_CAM, CLAMP_CAM));
-            camera.getTransform().setYaw(camera.getTransform().getYaw() + (xoffset * Timer.getDeltaTime() * .05f));
-            mousePosLast = input.getMousePos();
-            input.disableMouse();
-        }
-        else {
-            mouseFirst = true;
-            input.normalMouse();
-        }
+    public void onUpdate() {
+        if (Input.isKeyPressed(Input.KEY_ESCAPE)) quit();
+        if (Input.isKeyJustPressed(Input.KEY_F)) setFullscreen(!isFullscreen());
+        camera.update();
     }
 
     @Override
     public void onRender() {
-        camera.makeActive();
-        Framebuffer.drawLines();
-        camera.getScreenShader().ifPresent(
-                shader -> shader.setFloat2("resolution", getWindowWidth(), getWindowHeight())
-        );
-        entity.render();
-        Framebuffer.drawFaces();
+        camera.drawToFramebuffer(model);
         camera.render();
     }
 
     @Override
     public void onExit() {
-        entity.cleanup();
-        camera.cleanup();
+        model.destroyStack();
     }
 
-    public static void main(String[] args) {
-        CoffeeEngine.run(TestingBasicFunctionality.class);
+    public static void main(String... args) {
+        CoffeeEngine.run(new TestingBasicFunctionality());
     }
 }
